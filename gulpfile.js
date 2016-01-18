5255fc4b9fcd2b7aa70abcfe42b5feb1b6860b7e
Isigiel <lu.heddendorp@gmail.com>
2016-01-18 14:24:08 +0100

    Basic dev setup

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Lukas on 18.01.2016.
+ */
+// get the dependencies
+var gulp        = require('gulp'),
+    childProcess  = require('child_process'),
+    electron      = require('electron-prebuilt');
+
+// create the gulp task
+gulp.task('run', function () {
+     childProcess.spawn(electron, ['--debug=5858','./app'], { stdio: 'inherit' }); 
+});
